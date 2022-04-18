3dd5b5f01e2f398c3991845cbd82e31e1182efa6
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-04-18 13:41:50 -0700

    Added TODO

--- docker/pokt-net/dev-tm/entrypoint-dev.sh
+++ docker/pokt-net/dev-tm/entrypoint-dev.sh
@@ -8,6 +8,7 @@ if [ -n "${POCKET_CORE_GENESIS}" ] ; then
     echo "${POCKET_CORE_GENESIS}" > /home/app/.pocket/config/genesis.json
 fi
 
+# NOTE: The default `chains.json` does not support all the chains in Pocket mainnet.
 if [ -n "${POCKET_CORE_CHAINS}" ] ; then
     echo "${POCKET_CORE_CHAINS}" > /home/app/.pocket/config/chains.json
 fi
